225d45f28f604b6926b6353b2b299b425164d03c
jdob <jdobies@redhat.com>
2020-03-20 14:30:49 -0400

    Upgraded mongo driver and fixed db auth

--- src/main/java/com/redhat/osevg/concessions/ConcessionsController.java
+++ src/main/java/com/redhat/osevg/concessions/ConcessionsController.java
@@ -1,50 +1,60 @@
 package com.redhat.osevg.concessions;
 
 import com.mongodb.*;
+import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class ConcessionsController {
 
+    // These variables are introduced by the mongo secret, for example, linked via odo
+    // If they aren't set, no attempt will be made to connect to the DB
+    // Yes, they are lowercase in the secret :)
+    private static final String ENV_DB_URI = "uri";
+    private static final String ENV_DB_USERNAME = "username";
+    private static final String ENV_DB_PASSWORD = "password";
+    private static final String ENV_DB_NAME = "database_name";
+
     private MongoClient client;
+    private String databaseName;
+
     private Logger logger = LoggerFactory.getLogger(ConcessionsController.class);
 
     @GetMapping("/allorders")
-    public List<DBObject> allOrders() {
-        DBCollection collection = orders();
+    public List allOrders() {
+        MongoCollection collection = orders();
 
-        DBCursor allOrders = collection.find();
-        return allOrders.toArray();
+        List allOrders = new ArrayList();
+
+        MongoCursor<Document> cursor = collection.find().iterator();
+        while (cursor.hasNext()) {
+            Document d = cursor.next();
+            d.remove("_id");
+            allOrders.add(d);
+        }
+        return allOrders;
     }
 
     @GetMapping("/ticketNumber")
     public Map ticketNumber(@RequestParam Map<String, String> order) {
-        DBCollection collection = orders();
+        MongoCollection collection = orders();
 
-        int nextTicket = 100;
-
-        // Increment the ticket number from the last order if one is found
-        int count = collection.find().count();
-        if (count > 0) {
-            DBCursor cursor = collection.find().sort(new BasicDBObject("ticketNumber", -1)).limit(1);
-            DBObject lastOrder = cursor.next();
-            nextTicket = (int) lastOrder.get("ticketNumber") + 1;
-        }
+        // The JS backend started ticket numbers at 100 and incremented from there
+        long nextTicket = collection.countDocuments() + 100;
 
         // Add the order to the database
-        BasicDBObject document = new BasicDBObject();
-        document.put("ticketNumber", nextTicket);
-        document.put("order", order);
-        collection.insert(document);
+        Document document = new Document()
+            .append("ticketNumber", nextTicket)
+            .append("order", order);
+        collection.insertOne(document);
 
         // Package the response
         Map result = new HashMap();
@@ -57,71 +67,73 @@ public class ConcessionsController {
 
     @GetMapping("/debug")
     public Map debug() {
-        MongoClientURI mongoUri = mongoClientURI();
-
         boolean connected = false;
 
         try {
             MongoClient mongoClient = mongoClient();
-            mongoClient.getDatabaseNames();
+            mongoClient.listDatabaseNames();
             connected = true;
         } catch (MongoTimeoutException e) {
             // Expected if the DB isn't found
+            logger.info("Timeout trying to connect to database");
         }
 
         Map results = new HashMap();
-        results.put("mongo_url", mongoUri.getURI());
+        results.put("mongo_database", databaseName);
         results.put("connected", connected);
 
         return results;
     }
 
-    private DBCollection orders() {
-        MongoClientURI mongoUri = mongoClientURI();
+    private MongoCollection orders() {
         MongoClient mongoClient = mongoClient();
-        DB database = mongoClient.getDB(mongoUri.getDatabase());
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
         return database.getCollection("orders");
     }
 
     private MongoClient mongoClient() {
-        if (client == null) {
-            try {
-                client = new MongoClient(mongoClientURI());
-            } catch (UnknownHostException e) {
-                logger.error("Host not found", e);
-            }
+
+        // Lazy loading
+        if (client != null) {
+            return client;
         }
-        return client;
-    }
 
-    /**
-     * Reads the appropriate environment variables to determine the mongo URI.
-     */
-    private MongoClientURI mongoClientURI() {
-
-        // Default if it's not overridden from the environment
-        String uri = "mongodb://localhost:27017/sampledb";
-
-        // These variables are introduced by the mongo secret, for example, linked via odo
-        // Yes, they are lowercase in the secret :)
-        String envUri = System.getenv("uri");
+        // Simple check; assume the rest of the variables are set if the URI is
+        String envUri = System.getenv(ENV_DB_URI);
         if (envUri != null) {
-            String username = System.getenv("username");
-            String password = System.getenv("password");
-            String dbName = System.getenv("database_name");
-            String[] pieces = envUri.split("//");
-            uri = pieces[0] + "//" + username + ":" + password + "@" + pieces[1] + "/" + dbName;
-        } else if (System.getenv("MONGODB_URL") != null) {
-            uri = System.getenv("MONGODB_URL");
+
+            // Example uri value: mongodb://172.30.237.184:27017
+            String hostAndPort = envUri.split("//")[1];
+            String host = hostAndPort.split(":")[0];
+            int port = Integer.parseInt(hostAndPort.split(":")[1]);
+            String username = System.getenv(ENV_DB_USERNAME);
+            String password = System.getenv(ENV_DB_PASSWORD);
+            databaseName = System.getenv(ENV_DB_NAME);
+
+            logger.info("Host: {}", host);
+            logger.info("Port: {}", port);
+            logger.info("Username: {}", username);
+            logger.info("Password: {}", password);
+            logger.info("Database: {}", databaseName);
+
+            MongoCredential credentials =  MongoCredential.createCredential(
+                    username,
+                    databaseName,
+                    password.toCharArray());
+
+            client = MongoClients.create(
+                    MongoClientSettings.builder()
+                            .applyToClusterSettings(builder ->
+                                    builder.hosts(Arrays.asList(new ServerAddress(host, port))))
+                            .credential(credentials)
+                            .build());
+        }
+        else {
+            // If the environment variables aren't included, default to a simple local connection
+            client = MongoClients.create("mongodb://localhost:27017");
+            databaseName = "concessions";
         }
 
-        logger.info("Using {}", uri);
-
-        // Set to a low timeout since during the demo there is a case where we deploy this
-        // app without having the database, so no need to wait the default 10 seconds
-        MongoClientOptions.Builder optionsBuilder = MongoClientOptions.builder();
-        optionsBuilder.connectTimeout(2000);
-
-        return new MongoClientURI(uri, optionsBuilder);
+        return client;
     }
 }
