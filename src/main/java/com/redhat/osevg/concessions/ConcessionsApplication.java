e894b5e1c71bb38d0d5c7bee70b5c9dda45c7be0
jdob <jdobies@redhat.com>
2020-03-23 11:43:07 -0400

    No longer try to automatically connect to Mongo
    This suppresses the start up exception when the database can't be
    accessed, either because it's not linked or because the magic to pull
    the configuration from the env hasn't happened yet. We'll connect via
    lazy loading when it's needed.

--- src/main/java/com/redhat/osevg/concessions/ConcessionsApplication.java
+++ src/main/java/com/redhat/osevg/concessions/ConcessionsApplication.java
@@ -2,8 +2,10 @@ package com.redhat.osevg.concessions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ConcessionsApplication {
 
 	public static void main(String[] args) {
